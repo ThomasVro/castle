0da498247813bc1effbd3c5b9978a82b1bce1844
ThomasVro <vaquerothomas@gmail.com>
2019-02-18 15:26:20 +0100

    origin

--- Code/address.js
+++
@@ -1,7 +0,0 @@
-var michelin = require('./michelin');
-var jsonfile = require('jsonfile');
-
-jsonfile.readFile('output/1_restaurants_list.json', function (err, restaurants) {
-    //getting the address of the restaurants and put it in "liste2.json"
-    michelin.getAllAddresses(restaurants);
-});
